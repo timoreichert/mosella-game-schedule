b03e14faf32e60983323f930d0226b914bad6002
timoreichert <timoreichert@web.de>
2018-07-12 09:09:37 +0200

    parse fussball.de

--- src/main/java/de/schweich/mosella/GameScheduleController.java
+++ src/main/java/de/schweich/mosella/GameScheduleController.java
@@ -1,5 +1,14 @@
 package de.schweich.mosella;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringWriter;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.jsoup.Jsoup;
+import org.jsoup.select.Elements;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +17,41 @@ public class GameScheduleController {
 
     @RequestMapping("/next-games")
     public String nextGames() {
-        return "next games...";
+        StringWriter out = new StringWriter();
+        out.write("<pre><code>");
+        String spec = "http://www.fussball.de/ajax.club.next.games/-/id/00ES8GNB78000065VV0AG08LVUPGND5I";
+        try (InputStream in = new URL(spec).openStream()) {
+            Elements matchplan = Jsoup
+                    .parse(in, "UTF-8", "")
+                    .select(".club-matchplan-table")
+                    .select("table>tbody>tr");
+            matchplan.forEach(e -> {
+                if (e.hasClass("row-headline")) {
+                    //System.out.println(e.select("td").text());
+                } else if (e.hasClass("row-competition")) {
+                    if(e.select(".column-date").text().contains("|")){
+                        out.write("\n" + e.select(".column-date").text().replaceAll("\\s\\|{1}\\s", "\n"));
+                    }else{
+                        out.write(e.select(".column-date").text());
+                    }
+                    out.write(" | " + e.select(".column-team>a").text() + '\n');
+                } else {
+                    e.select("td").forEach((c -> {
+                        if (c.hasClass("column-club")) {
+                            out.write(c.select(".club-name").text());
+                        } else if (c.hasClass("column-colon")) {
+                            out.write(" " + c.text() + " ");
+                        } else if (c.hasClass("column-score")) {
+                            out.write('\n');
+                        }
+                    }));
+                }
+            });
+        } catch (IOException e) {
+            Logger.getLogger("MOSELLA").log(Level.SEVERE, null, e);
+        }
+        out.write("</code></pre>");
+        out.flush();
+        return out.toString();
     }
 }
