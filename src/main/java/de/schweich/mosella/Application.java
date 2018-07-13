2e8b85e137537c0605449641e9bc394bb691560d
ga2remo <ga2remo@AP721247.ztb.icb.commerzbank.com>
2018-07-13 16:29:55 +0200

    remove proxy

--- src/main/java/de/schweich/mosella/Application.java
+++ src/main/java/de/schweich/mosella/Application.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
-        
-        System.setProperty("java.net.useSystemProxies", "true");
         SpringApplication.run(Application.class, args);
     }
 }
