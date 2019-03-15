188e30f4340c3cfce1151ffc625d6fc91f46fa14
timoreichert <timoreichert@web.de>
2019-03-15 14:56:53 +0100

    fix matchplan

--- src/main/java/de/schweich/mosella/GameScheduleController.java
+++ src/main/java/de/schweich/mosella/GameScheduleController.java
@@ -4,7 +4,11 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringWriter;
 import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Locale;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.jsoup.Jsoup;
@@ -17,25 +21,44 @@ public class GameScheduleController {
 
     @RequestMapping("/next-games")
     public String nextGames() {      
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+        Calendar cal = Calendar.getInstance(Locale.GERMANY);
+        cal.set(Calendar.HOUR_OF_DAY, 0);
+        cal.set(Calendar.MINUTE, 0);
+        cal.set(Calendar.SECOND, 0);
+        cal.set(Calendar.MILLISECOND, 0);
+
         StringWriter out = new StringWriter();
-        
         out.write("<h2>Spielvorschau Abteilung Fußball KW " 
-                + Calendar.getInstance().get(Calendar.WEEK_OF_YEAR)
-                +  "/"
-                + Calendar.getInstance().get(Calendar.YEAR)
+                + cal.get(Calendar.WEEK_OF_YEAR) + "/" + cal.get(Calendar.YEAR)
                 + "</h2>");
         out.write("\n<p>Alle wichtigen Termine der kommenden Woche auf eine Blick</p>"
                 + "\n\t<ul>"); 
         
-        String spec = "http://www.fussball.de/ajax.club.next.games/-/id/00ES8GNB78000065VV0AG08LVUPGND5I";
+        int dayFromMonday = (cal.get(Calendar.DAY_OF_WEEK) + 7 - Calendar.MONDAY) % 7;
+        cal.add(Calendar.DATE, -dayFromMonday);
+        String from = df.format(cal.getTime());
+        cal.add(Calendar.DATE, 7);
+        cal.add(Calendar.MILLISECOND, -1);
+        String to = df.format(cal.getTime());
+
+        String spec = "http://www.fussball.de/ajax.club.matchplan/-/"
+                + "id/00ES8GNB78000065VV0AG08LVUPGND5I/"
+                + "mime-type/HTML/"
+                + "mode/PAGE/"
+                + "show-filter/false/"
+                + "max/50/"
+                + "datum-von/"+from+"/"
+                + "datum-bis/"+to+"/"
+                + "offset/0";
         try (InputStream in = new URL(spec).openStream()) {
+            
             Elements matchplan = Jsoup
                     .parse(in, "UTF-8", "")
                     .select(".club-matchplan-table")
                     .select("table>tbody>tr");
             
             matchplan.forEach(e -> {
-                
                 if (e.hasClass("row-headline")) {
                     //System.out.println(e.select("td").text());
                 } else if (e.hasClass("row-competition")) {
@@ -68,7 +91,6 @@ public class GameScheduleController {
                     }));
                     out.write("\t</pre></li>\n");
                 }
-                
             });
         } catch (IOException e) {
             Logger.getLogger("MOSELLA").log(Level.SEVERE, null, e);
